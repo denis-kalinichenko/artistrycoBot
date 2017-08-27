69a203faf2419126075d5779310cae12c1bd5c96
Denys Kalinichenko <it51@g2a.com>
2017-08-27 20:10:07 +0200

    basic functions

--- Bot.js
+++ Bot.js
@@ -3,18 +3,21 @@ const validUrl = require("valid-url");
 const parseDomain = require("parse-domain");
 
 const config = require("./config");
-const product = require("./src/product");
+const Product = require("./src/product");
 
 const bot = new TelegramBot(config.token, {polling: true});
+
+let product = null;
+let watcher;
 
 bot.on("message", msg => {
     bot.sendChatAction(msg.chat.id, "typing");
 
-    const AKS_LINK_MESSAGE = "Send me link to the product which I should add to the cart.";
-
-    if (msg.text === "/start") {
+    if (msg.entities && msg.entities[0].type === "bot_command") {
         return;
     }
+
+    const AKS_LINK_MESSAGE = "Send me link to the product which I should add to the cart.";
 
     if (!validUrl.isWebUri(msg.text)) {
         return bot.sendMessage(msg.chat.id, AKS_LINK_MESSAGE);
@@ -25,16 +28,131 @@ bot.on("message", msg => {
     if (parsedDomain !== "theartistryco.com") {
         return bot.sendMessage(msg.chat.id, "Please send a link to a product from www.theartistryco.com website only.");
     }
-    product.get(msg.text, (error, $) => {
+
+    product = new Product();
+
+    product.load(msg.text, error => {
         if (error) {
             return bot.sendMessage(msg.chat.id, error);
         }
-        bot.sendMessage(msg.chat.id, product.isSoldOut($));
+
+        const response = `${product.name}\n\nPrice: ${product.price}\n\n${(product.soldOut) ? "❌ Sold Out" : "✅ In Stock"}`;
+        let reply_markup;
+
+        if (!product.soldOut)  {
+            reply_markup = {
+                keyboard: [["/add"]],
+            };
+        } else {
+            reply_markup = {
+                keyboard: [["/watch"]],
+            };
+        }
+
+        bot.sendPhoto(msg.chat.id, product.image, {
+            caption: response,
+            reply_markup: reply_markup,
+        });
     });
 });
 
 bot.onText(/\/start/, msg => {
     let message_body = `Hi, ${msg.chat.first_name}. Send me link to the product which I should add to the cart.`;
 
+    product = null;
+
     bot.sendMessage(msg.chat.id, message_body);
+});
+
+bot.onText(/\/add/, msg => {
+    if (!product) {
+        return;
+    }
+
+    product.addToCart(() => {
+        let message_body = `Product has been successfully added to Cart! Copy the session code:\n\n<pre>${product.session}</pre>` +
+        `\n\nAnd then open www.theartistryco.com/cart in your browser, change the cookie value of <b>_big_cartel_session</b> `+
+        `and refresh the page. Send /help command for more information about browser cookies.`;
+        bot.sendMessage(msg.chat.id, message_body, {
+            parse_mode: "HTML",
+            disable_web_page_preview: true,
+            reply_markup: {hide_keyboard: true},
+        });
+    });
+});
+
+bot.onText(/\/watch/, msg => {
+    if (!product) {
+        return;
+    }
+
+    if (!product.soldOut) {
+        bot.sendMessage(msg.chat.id, "Product is already in stock. Send /add command to add product to the Cart.", {
+            reply_markup: {hide_keyboard: true},
+        });
+        return;
+    }
+
+    bot.sendMessage(msg.chat.id, `⌛ Starting watching product... Update each 3s.`, {
+        reply_markup: {
+            keyboard: [["/stop"]],
+        },
+    });
+
+    watcher = setInterval(() => {
+        bot.sendChatAction(msg.chat.id, "typing");
+
+        product.load(product.url, error => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            console.log(!product.soldOut);
+
+            if (!product.soldOut) {
+                bot.sendMessage(msg.chat.id, "Product became available! Adding to Cart...", {
+                    reply_markup: {hide_keyboard: true},
+                });
+
+                clearInterval(watcher);
+
+                product.addToCart(() => {
+                    let message_body = `<b>Yeah.</b> Product has been successfully added to Cart! Copy the session code:\n\n<pre>${product.session}</pre>` +
+                        `\n\nAnd then open www.theartistryco.com/cart in your browser, change the cookie value of <b>_big_cartel_session</b> `+
+                        `and refresh the page. Send /help command for more information about browser cookies.`;
+                    bot.sendMessage(msg.chat.id, message_body, {
+                        parse_mode: "HTML",
+                        disable_web_page_preview: true,
+                        reply_markup: {hide_keyboard: true},
+                    });
+                });
+            }
+        });
+
+    }, 3000);
+});
+
+bot.onText(/\/stop/, msg => {
+    if (!product) {
+        return;
+    }
+
+    if (!watcher) {
+        return;
+    }
+
+    clearInterval(watcher);
+
+    bot.sendMessage(msg.chat.id, "Watcher has been stopped.", {
+        reply_markup: {hide_keyboard: true},
+    });
+});
+
+bot.onText(/\/help/, msg => {
+    bot.sendDocument(msg.chat.id, "http://i.imgur.com/zBNmPRX.gif", {
+        caption: `To edit cookies go to www.editthiscookie.com and install extension for Google Chrome, ` +
+        `use it to change the value of '_big_cartel_session' cookie. Refresh the page.`,
+        reply_markup: { hide_keyboard: true },
+    });
 });
--- src/product.js
+++ src/product.js
@@ -1,31 +1,92 @@
 const http = require("http");
+const querystring = require("querystring");
 const cheerio = require("cheerio");
+const cookieparser = require("cookieparser");
 
-function get(url, callback) {
-    return http.get(url, response => {
-        const { statusCode } = response;
+class Product {
+    constructor() {
+        this.$ = null;
+        this.soldOut = null;
+        this.name = null;
+        this.price = null;
+        this.image = null;
+        this.session = null;
+        this.url = null;
+    }
 
-        if (statusCode !== 200) {
-            return callback("Can't load page. Status code: " + statusCode);
+    load(url, callback) {
+        this.url = url;
+        return http.get(url, response => {
+            const { statusCode } = response;
+
+            if (statusCode !== 200) {
+                return callback("Can't load page. Status code: " + statusCode);
+            }
+
+            response.setEncoding('utf8');
+            let body = "";
+            response.on("data", chunk => { body += chunk; });
+            response.on("end", () => {
+                this.$ = cheerio.load(body);
+                this._parse();
+                callback(null);
+            });
+        }).on('error', error => {
+            callback(`Got error: ${error.message}`);
+        });
+    }
+
+    addToCart(callback) {
+        if (this.soldOut) {
+            return;
         }
 
-        response.setEncoding('utf8');
-        let body = "";
-        response.on("data", chunk => { body += chunk; });
-        response.on("end", () => {
-            const $ = cheerio.load(body);
-            callback(null, $);
+        const id = this.$("#option").attr("value");
+
+        const post_data = querystring.stringify({
+            utf8: "✓",
+            "cart[add][id]": id,
+            submit: "",
         });
-    }).on('error', error => {
-        callback(`Got error: ${error.message}`);
-    });
+
+        const post_options = {
+            host: "www.theartistryco.com",
+            port: "80",
+            path: "/cart",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Content-Length": Buffer.byteLength(post_data),
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+            }
+        };
+
+        let data = "";
+
+        const request = http.request(post_options, res => {
+            res.setEncoding('utf8');
+
+            res.on('data', chunk => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                const cookies = cookieparser.parse(res.headers["set-cookie"][0]);
+                this.session = cookies._big_cartel_session;
+                callback();
+            });
+        });
+
+        request.write(post_data);
+        request.end();
+    }
+
+    _parse() {
+        this.soldOut = this.$("h4.button.disabled").text() === "Sold Out";
+        this.name = this.$("h1").text();
+        this.price = this.$("header.product_header h3").text();
+        this.image = this.$("#image_1 img").attr("src");
+    }
 }
 
-function isSoldOut($) {
-    return $("h4.button.disabled").text() === "Sold Out";
-}
-
-module.exports = {
-    get,
-    isSoldOut,
-};
+module.exports = Product;
